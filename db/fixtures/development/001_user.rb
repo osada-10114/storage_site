3d036decfc2062bae62a534e9ee238423cb25c01
TakaoOsada <try-osada@yahoo.ne.jp>
2018-03-14 20:09:32 +0900

    removed_gender_column_from_users

--- db/fixtures/development/001_user.rb
+++ db/fixtures/development/001_user.rb
@@ -2,7 +2,6 @@ User.seed_once(:id) do |user|
 	user.id = 1
 	user.nickname =  "testuser"
 	user.email = "testuser@example.com"
-	user.gender = 0
 	user.password = "testuser"
 	user.password_confirmation = "testuser"
 end
